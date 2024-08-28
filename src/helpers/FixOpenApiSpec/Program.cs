76e8e165b36c326475d591e13b27dfd749ab2222
HavenDV <havendv@gmail.com>
2024-08-28 14:40:57 +0400

    fix: Fixed issue with OpenAPI 3.1.0.

--- src/helpers/FixOpenApiSpec/Program.cs
+++ src/helpers/FixOpenApiSpec/Program.cs
@@ -8,7 +8,8 @@ var path = args[0];
 var text = await File.ReadAllTextAsync(path);
 
 text = text
-    .Replace(",\"/studio/v1/custom-model/{model_pid}\":{\"delete\":{\"tags\":[\"custom-models\"],\"summary\":\"Delete Model\",\"operationId\":\"v1_delete_custom_model\",\"parameters\":[{\"required\":true,\"schema\":{\"title\":\"Model Pid\",\"type\":\"string\"},\"name\":\"model_pid\",\"in\":\"path\"}],\"responses\":{\"200\":{\"description\":\"Successful Response\",\"content\":{\"application/json\":{\"schema\":{}}}},\"422\":{\"description\":\"Validation Error\",\"content\":{\"application/json\":{\"schema\":{\"$ref\":\"#/components/schemas/HTTPValidationError\"}}}}}}}", string.Empty)
+    .Replace("\"openapi\":\"3.1.0\"", "\"openapi\":\"3.0.1\"")
+    .Replace(",\"/studio/v1/custom-model/{model_pid}\":{\"delete\":{\"tags\":[\"custom-models\"],\"summary\":\"Delete Model\",\"operationId\":\"v1_delete_custom_model\",\"parameters\":[{\"required\":true,\"schema\":{\"type\":\"string\",\"title\":\"Model Pid\"},\"name\":\"model_pid\",\"in\":\"path\"}],\"responses\":{\"200\":{\"description\":\"Successful Response\",\"content\":{\"application/json\":{\"schema\":{}}}},\"422\":{\"description\":\"Validation Error\",\"content\":{\"application/json\":{\"schema\":{\"$ref\":\"#/components/schemas/HTTPValidationError\"}}}}}}}", string.Empty)
     ;
 var openApiDocument = new OpenApiStringReader().Read(text, out var diagnostics);
 
