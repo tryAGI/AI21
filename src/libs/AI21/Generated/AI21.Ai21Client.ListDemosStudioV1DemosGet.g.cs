b1f3663c0384c9f5a9e101cc41dad0496d9b5f2d
Konstantin S. <havendv@gmail.com>
2026-03-29 01:38:44 +0400

    feat: Updated OpenAPI spec (#283)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/AI21/Generated/AI21.Ai21Client.ListDemosStudioV1DemosGet.g.cs
+++ src/libs/AI21/Generated/AI21.Ai21Client.ListDemosStudioV1DemosGet.g.cs
@@ -102,7 +102,7 @@ namespace AI21
                     __response.EnsureSuccessStatusCode();
 
                     return
-                        global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::AI21.Demo>), JsonSerializerContext) as global::System.Collections.Generic.IList<global::AI21.Demo> ??
+                        (global::System.Collections.Generic.IList<global::AI21.Demo>?)global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::AI21.Demo>), JsonSerializerContext) ??
                         throw new global::System.InvalidOperationException($"Response deserialization failed for \"{__content}\" ");
                 }
                 catch (global::System.Exception __ex)
@@ -133,7 +133,7 @@ namespace AI21
                     ).ConfigureAwait(false);
 
                     return
-                        await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::AI21.Demo>), JsonSerializerContext).ConfigureAwait(false) as global::System.Collections.Generic.IList<global::AI21.Demo> ??
+                        (global::System.Collections.Generic.IList<global::AI21.Demo>?)await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::AI21.Demo>), JsonSerializerContext).ConfigureAwait(false) ??
                         throw new global::System.InvalidOperationException("Response deserialization failed.");
                 }
                 catch (global::System.Exception __ex)
--- src/libs/AI21/Generated/AI21.LibraryManagementClient.V1LibraryManagement.g.cs
+++ src/libs/AI21/Generated/AI21.LibraryManagementClient.V1LibraryManagement.g.cs
@@ -189,7 +189,7 @@ namespace AI21
                     __response.EnsureSuccessStatusCode();
 
                     return
-                        global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::AI21.FileResponse>), JsonSerializerContext) as global::System.Collections.Generic.IList<global::AI21.FileResponse> ??
+                        (global::System.Collections.Generic.IList<global::AI21.FileResponse>?)global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::AI21.FileResponse>), JsonSerializerContext) ??
                         throw new global::System.InvalidOperationException($"Response deserialization failed for \"{__content}\" ");
                 }
                 catch (global::System.Exception __ex)
@@ -220,7 +220,7 @@ namespace AI21
                     ).ConfigureAwait(false);
 
                     return
-                        await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::AI21.FileResponse>), JsonSerializerContext).ConfigureAwait(false) as global::System.Collections.Generic.IList<global::AI21.FileResponse> ??
+                        (global::System.Collections.Generic.IList<global::AI21.FileResponse>?)await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::AI21.FileResponse>), JsonSerializerContext).ConfigureAwait(false) ??
                         throw new global::System.InvalidOperationException("Response deserialization failed.");
                 }
                 catch (global::System.Exception __ex)
