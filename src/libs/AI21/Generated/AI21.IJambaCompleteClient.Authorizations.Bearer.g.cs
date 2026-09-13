46eaaff2359cec7dc2fd9603e7481c1ed1da2868
Konstantin S. <havendv@gmail.com>
2026-09-13 08:33:42 +0400

    feat: Updated OpenAPI spec (#372)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/AI21/Generated/AI21.IJambaCompleteClient.Authorizations.Bearer.g.cs
@@ -0,0 +1,16 @@
+
+#nullable enable
+
+namespace AI21
+{
+    public partial interface IJambaCompleteClient
+    {
+        /// <summary>
+        /// Authorize using bearer authentication.
+        /// </summary>
+        /// <param name="apiKey"></param>
+
+        public void AuthorizeUsingBearer(
+            string apiKey);
+    }
+}
---
+++ src/libs/AI21/Generated/AI21.LibraryManagementClient.Authorizations.Bearer.g.cs
@@ -0,0 +1,35 @@
+
+#nullable enable
+
+namespace AI21
+{
+    public sealed partial class LibraryManagementClient
+    {
+
+        /// <inheritdoc/>
+        public void AuthorizeUsingBearer(
+            string apiKey)
+        {
+            apiKey = apiKey ?? throw new global::System.ArgumentNullException(nameof(apiKey));
+
+            for (var i = Authorizations.Count - 1; i >= 0; i--)
+            {
+                var __authorization = Authorizations[i];
+                if (__authorization.Type == "Http" &&
+                    __authorization.Name == "Bearer")
+                {
+                    Authorizations.RemoveAt(i);
+                }
+            }
+
+            Authorizations.Add(new global::AI21.EndPointAuthorization
+            {
+                Type = "Http",
+                SchemeId = "HttpBearer",
+                Location = "Header",
+                Name = "Bearer",
+                Value = apiKey,
+            });
+        }
+    }
+}
